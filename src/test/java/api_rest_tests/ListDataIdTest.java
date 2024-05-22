4b4af7b37a34a50b31e1120141659969e686b9de
Anna Usolskaya <rakeya@yandex.ru>
2024-05-22 16:33:15 +0300

    fix3

--- src/test/java/api_rest_tests/ListDataIdTest.java
+++ src/test/java/api_rest_tests/ListDataIdTest.java
@@ -1,23 +1,18 @@
 package api_rest_tests;
 
-import io.restassured.RestAssured;
 import models.lombok.IdDataModel;
 import models.lombok.IdModelDataItem;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicReference;
 
 import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static specs.SetNameAndJobSpec.ListDataIdRequestSpec;
 
 public class ListDataIdTest {
-
 
 
     @Test
@@ -52,8 +47,8 @@ public class ListDataIdTest {
 //                .body("data.id", is(List.of(1, 2, 3, 4, 5, 6)));
 //
 
-}
-//    @Test
+
+    //    @Test
 //    void listDataIdTest() {
 //
 //        var request = given()
@@ -72,3 +67,31 @@ public class ListDataIdTest {
 //
 //
 //    }
+    @Test
+    void listDataIdTest() {
+
+
+                    var response = given(ListDataIdRequestSpec)
+
+
+                            .when()
+                            .get("https://reqres.in/api/unknown")
+
+                            .then()
+                            .log().status()
+                            .log().body()
+                            .statusCode(200)
+                            .extract().as(IdDataModel.class);
+
+                    List<Integer> ids = new ArrayList<>();
+                    for (IdModelDataItem item : response.getData()) {
+                        ids.add(item.getId());
+                    }
+        List<Integer> result = ids;
+                    // return   response.getData().stream().map(IdModelDataItem::getId).toList();
+
+
+
+                assertEquals(result, List.of(1, 2, 3, 4, 5, 6));
+    }
+}

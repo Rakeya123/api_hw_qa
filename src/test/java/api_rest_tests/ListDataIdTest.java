f3ae8ffdda0c6a814402b7c14383ad3b4e83e7ee
Anna Usolskaya <rakeya@yandex.ru>
2024-05-22 17:24:18 +0300

    fix4

--- src/test/java/api_rest_tests/ListDataIdTest.java
+++ src/test/java/api_rest_tests/ListDataIdTest.java
@@ -1,7 +1,5 @@
 package api_rest_tests;
 
-import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.selenide.AllureSelenide;
 import models.lombok.IdDataModel;
 import models.lombok.IdModelDataItem;
 import org.junit.jupiter.api.BeforeAll;
@@ -18,10 +16,6 @@ import static specs.SetNameAndJobSpec.ListDataIdRequestSpec;
 
 public class ListDataIdTest {
 
-@BeforeAll static void setup() {
-    SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-
-    }
     @Test
     void listDataIdTestAllure() {
         List<Integer> result =
@@ -74,31 +68,5 @@ public class ListDataIdTest {
 //
 //
 //    }
-    @Test
-    void listDataIdTest() {
 
-
-                    var response = given(ListDataIdRequestSpec)
-
-
-                            .when()
-                            .get("https://reqres.in/api/unknown/")
-
-                            .then()
-                            .log().status()
-                            .log().body()
-                            .statusCode(200)
-                            .extract().as(IdDataModel.class);
-
-                    List<Integer> ids = new ArrayList<>();
-                    for (IdModelDataItem item : response.getData()) {
-                        ids.add(item.getId());
-                    }
-        List<Integer> result = ids;
-                    // return   response.getData().stream().map(IdModelDataItem::getId).toList();
-
-
-
-                assertEquals(result, List.of(1, 2, 3, 4, 5, 6));
-    }
 }
